12b61867f1fc21e9554f859cf38072278fb86285
Edgard Souza <edgard.souzalopes@gmail.com>
2022-12-19 13:41:13 -0300

    Galeria

--- js/script.js
+++ js/script.js
@@ -14,18 +14,22 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 
+
 //* Ativar Itens do Orçamento
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
 
   const elemento = document.getElementById(parametro);
+
   if (elemento) {
      elemento.checked = true;
   };
 };
 
 parametros.forEach(ativarProduto);
+
+
 
 //* Perguntas Frequentes
 const perguntas = document.querySelectorAll(".perguntas button");
@@ -39,10 +43,34 @@ function ativarPerguntas(event) {
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expanded", ativa);
-}
+};
 
 function eventosPerguntas(pergunta) {
+
   pergunta.addEventListener('click', ativarPerguntas);
 };
 
 perguntas.forEach(eventosPerguntas);
+
+
+
+//* Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens")
+
+function trocarImagem(event) {
+
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  };
+};
+
+function eventosGaleria(img) {
+  
+  img.addEventListener('click', trocarImagem);
+};
+
+galeria.forEach(eventosGaleria);
