9478267a5b1f73e1ebb0aae5cedc29eda78d750e
Edgard Souza <edgard.souzalopes@gmail.com>
2022-12-19 10:10:26 -0300

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -13,15 +13,36 @@ function ativarLink(link) {
 
 links.forEach(ativarLink);
 
-//* Ativar Itens do Orçamento
 
+//* Ativar Itens do Orçamento
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
+
   const elemento = document.getElementById(parametro);
   if (elemento) {
      elemento.checked = true;
-  }
+  };
 };
 
 parametros.forEach(ativarProduto);
+
+//* Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPerguntas(event) {
+
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPerguntas);
+};
+
+perguntas.forEach(eventosPerguntas);
